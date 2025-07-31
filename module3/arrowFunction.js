cdcebe65776faa99033c500f2ce0f3d521a2a9d5
akhilaGuda <akhila.guda@beautifulcode.in>
2025-07-31 15:45:59 +0530

    module3 activities

---
+++ module3/arrowFunction.js
@@ -0,0 +1,3 @@
+const subtract = (first_num, second_num) => console.log(first_num - second_num);
+
+subtract(10,1);
---
+++ module3/stringReverse.js
@@ -0,0 +1,4 @@
+const stringReverse=(function reverseStr(value){
+    let charArray=value.split("").reverse();
+    console.log(charArray.join(""));
+})("Hello BC!!");
