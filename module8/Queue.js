5f19f7bea81e831e92cc12a88f2beaa31f57979d
akhilaGuda <akhila.guda@beautifulcode.in>
2025-08-05 13:12:05 +0530

    added instanceof

--- module8/Queue.js
+++ module8/Queue.js
@@ -1,36 +1,40 @@
-class Queue{
-    constructor(){
-        this.items=[];
+class Queue {
+    constructor() {
+        this.items = [];
     }
-  
-    enqueue(element){
+
+    enqueue(element) {
         this.items.push(element);
     }
-    
-    dequeue(){
-        if(this.isEmpty())return null;
+
+    dequeue() {
+        if (this.isEmpty()) return null;
         return this.items.shift(); // remove element from front
     }
-    front(){
-        if(this.isEmpty())return null;
+    front() {
+        if (this.isEmpty()) return null;
         return this.items[0];//peek from front
     }
-    isEmpty(){
-        return this.items.length===0;
+    isEmpty() {
+        return this.items.length === 0;
     }
-    display(){
+    display() {
         console.log(this.items);
     }
 }
 
-const queue=new Queue();
-console.log("created: ",queue);
+const queue = new Queue();
+console.log("created: ", queue);
 console.log("Queue is empty ? ", queue.isEmpty());
 queue.enqueue(7);
 queue.enqueue(20);
 queue.enqueue(8);
 queue.enqueue(1);
 queue.display();
-console.log("Dequeue element : ",queue.dequeue());
+console.log("Dequeue element : ", queue.dequeue());
 queue.display();
-console.log("Front element of queue: ",queue.front());
+console.log("Front element of queue: ", queue.front());
+console.log(queue instanceof Queue);
+// The instanceof operator checks whether an object is an instance of  a specific constructor or class.
+//  syntax: object instanceof Constructor. return true is the object created using the constructor or its prototype is in the prototype chain 
+// otherwise false.
