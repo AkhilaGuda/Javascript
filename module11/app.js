e18ae58a89414e7225be093e40ba4c45fe786de1
akhilaGuda <akhila.guda@beautifulcode.in>
2025-08-07 10:35:12 +0530

    import & export activity

---
+++ module11/app.js
@@ -0,0 +1,6 @@
+import {add,subtract} from './mathUtils.js';//importing named exports
+import multiply from './mathUtils.js';//importing default export
+//using imported functions
+console.log(add(5,7));
+console.log(subtract(20,7));
+console.log(multiply(2,7));
---
+++ module11/mathUtils.js
@@ -0,0 +1,10 @@
+export function add(a, b) {//named export
+    return a + b;
+}
+export function subtract(a, b) {//named export
+    return a - b;
+}
+export default function multiply(a, b){//default export
+     return a * b;
+}
+
